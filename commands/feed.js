df578a7adccee99b34637c05c3521a60a70b64fa
iamgabrieltv <gabrielsarcevic1@gmail.com>
2023-03-27 18:15:48 +0200

    optimized

--- commands/feed.js
+++ commands/feed.js
@@ -6,28 +6,28 @@ module.exports = {
 	async execute(interaction) {
 		switch (hungyLevel.getLevel()) {
 			case hungyLevel.getLevel() >= 50:
+				if (hungyLevel.getLevel() <= 90) hungyLevel.update();
+
 				interaction.reply(
 					`Nom Nom... Thank you <@${
 						interaction.member.id
 					}>! *Hungylevel: ${hungyLevel.getLevel()}%*`,
 				);
 
-				if (hungyLevel.getLevel() <= 90) hungyLevel.update();
-
 				break;
 			case hungyLevel.getLevel() <= 40:
+				if (hungyLevel.getLevel() <= 90) hungyLevel.update();
+
 				interaction.reply(
 					`I was starting to get hungy. Mau! *Hungylevel: ${hungyLevel.getLevel()}%*`,
 				);
 
-				if (hungyLevel.getLevel() <= 90) hungyLevel.update();
-
 				break;
 
 			case hungyLevel.getLevel() <= 20:
-				interaction.reply(`I am starving! *Hungylevel: ${hungyLevel.getLevel()}%*`);
-
 				if (hungyLevel.getLevel() <= 90) hungyLevel.update();
+
+				interaction.reply(`I am starving! *Hungylevel: ${hungyLevel.getLevel()}%*`);
 
 				break;
 		}
